07557a9e810af56f9f26d248fe81f8078d132777
tushar163 <tusharunofficial02@gmail.com>
2025-11-22 11:07:19 +0530

    code fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='h-screen'>page</div>
+    <div className='min-h-screen flex items-center justify-center w-full'>
+      <div className='h-[700px] relative overflow-hidden text-default'>
+        <div className='absolute inset-0'>
+          <div className='h-full w-full bg-'></div>
+          <div className="absolute inset-0 bg-black/30"></div>
+        </div>
+        <div className='relative z-10 flex flex-col justify-center items-center h-full px-4 max-w-[700px] mx-auto gap-6'>
+          <h1 className='relative text-4xl font-merriweather font-bold text-center text-white'>About Vishwa Guru <span className='text-primary'>Bharat</span></h1>
+          <p className='relative text-xl font-merriweather font-bold text-center text-white'>“A guru is someone who has the power to see greatness even in the lowest man and has the power to raise it.”</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
